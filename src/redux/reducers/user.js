4f78f970a67317dac1e04d1de2dcacc279199e7d
liciniopsj <licinio.psj@gmail.com>
2022-12-08 15:46:12 -0300

    Initial Commit | Redux Setup - store OK, reducers initial setup OK | App , Login , Wallet Connected to store | Components and Pages renamed to the JSX file extension

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+  },
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ACTION':
+    return {
+      ...state,
+    };
+
+  default:
+    return state;
+  }
+}
